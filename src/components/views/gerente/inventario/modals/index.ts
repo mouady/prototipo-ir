5a1470a278c778982b7b431b0124a40db2e37aab
Mohamed Ahmed <mohamedtaha2005@gmail.com>
2025-12-20 23:32:37 +0100

    muchos modals

--- src/components/views/gerente/inventario/modals/index.ts
+++ src/components/views/gerente/inventario/modals/index.ts
@@ -1,3 +1,6 @@
 export { PlatoModal } from "./PlatoModal";
 export { EliminarPlatoModal } from "./EliminarPlatoModal";
 export { FormatosSection } from "./FormatosSection";
+export { AgregarProductoModal } from "./AddModal";
+export { DeleteModal } from "./DeleteModal";
+export { LotesModal } from "./LotesModal";
