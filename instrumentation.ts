60ff0607628fab62edce80f4f75f0995e9afe321
Mohamed Ahmed <mohamedtaha2005@gmail.com>
2026-01-09 19:06:54 +0100

    Instrumentacion básica

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,6 @@
 import { registerOTel } from '@vercel/otel'
  
 export function register() {
-  registerOTel({ serviceName: 'prototipo-ir-otel' })
+  console.log('[OPENTELEMETRY] Registering instrumentation')
+  registerOTel({ serviceName: 'prototipo-ir' })
 }
